367b832e6376c793263603f059c7c811c5ae27e3
Felix <fhardjana@gmail.com>
2026-07-01 15:45:30 -0400

    moved file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import qrImage from './images/image-qr-code.png';
 
 const App = () => {
   return(
     <>
       <main>
         <div className="main-div-container">
-          <img id="qr-code-image" src="./src/images/image-qr-code.png" alt="qr-code-image" />
+          <img id="qr-code-image" src={qrImage} alt="qr-code-image" />
           <h1>Improve your front-end skills by building projects</h1>
           <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
