228e7fa9f50eb687d16c312a0f4fc2061da3096b
Felix <fhardjana@gmail.com>
2026-07-01 15:21:49 -0400

    moved file

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
     <>
       <main>
         <div className="main-div-container">
-          <img id="qr-code-image" src="./images/image-qr-code.png" alt="qr-code-image" />
+          <img id="qr-code-image" src="./src/images/image-qr-code.png" alt="qr-code-image" />
           <h1>Improve your front-end skills by building projects</h1>
           <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
